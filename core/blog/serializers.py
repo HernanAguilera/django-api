84b99e446749e6c3845c1a4cb1e8973417473bcf
Hernan Aguilera <haguilera@protonmail.com>
2024-05-24 13:43:30 -0600

    Created posts crud

--- core/blog/serializers.py
+++ core/blog/serializers.py
@@ -6,7 +6,10 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username']
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
--- core/blog/views/posts.py
+++ core/blog/views/posts.py
@@ -1,39 +1,49 @@
-
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from ..serializers import PostSerializer
+
 from blog.models import Post
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def index(request):
     posts = Post.objects.all()
-    return Response({'posts': posts})
+    serializer = PostSerializer(posts, many=True)
+    return Response({"posts": serializer.data})
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
-    return Response({'post': post})
+    serializer = PostSerializer(post)
+    return Response({"post": serializer.data})
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def post_create(request):
-    data = request.data
+    data = {**request.data, "user_id": request.user.id}
+    tags = data.pop("tags") if "tags" in data else []
     post = Post.objects.create(**data)
-    return Response({'post': post})
+    if tags:
+        post.tags.set(tags)
+    serializer = PostSerializer(post)
+    return Response({"post": serializer.data})
 
 
-@api_view(['PUT'])
+@api_view(["PUT"])
 def post_update(request, pk):
     data = request.data
     post = Post.objects.get(pk=pk)
-    post.title = data['title']
-    post.content = data['content']
+    post.title = data["title"]
+    post.content = data["content"]
     post.save()
-    return Response({'post': post})
+    serializer = PostSerializer(post)
+    return Response({"post": serializer.data})
 
 
-@api_view(['DELETE'])
+@api_view(["DELETE"])
 def post_delete(request, pk):
     post = Post.objects.get(pk=pk)
     post.delete()
-    return Response({'message': 'Post deleted successfully!'})
+    return Response({"message": "Post deleted successfully!"})
