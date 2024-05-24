fbf912d9eba8a08609434965a2d436eca3a7b016
Hernan Aguilera <haguilera@protonmail.com>
2024-05-24 13:42:53 -0600

    Added routes

--- core/core/urls.py
+++ core/core/urls.py
@@ -14,13 +14,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('blog.urls')),
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
-     path('api-auth/', include('rest_framework.urls'))
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path(
+        "api/",
+        include(
+            [
+                path("auth/", include("auth.urls")),
+                path("blog/", include("blog.urls")),
+            ]
+        ),
+    ),
 ]
